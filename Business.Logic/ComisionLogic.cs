5a1760f5ab461f334b4b1ec3c2c909e4d7d391c2
AlexisTomas2000 <alexisjosetomas@gmail.com>
2021-07-25 10:31:13 -0300

    Clase ComLogic

--- Business.Logic/ComisionLogic.cs
+++ Business.Logic/ComisionLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Business.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,44 @@ namespace Business.Logic
 {
     public class ComisionLogic
     {
+            private Data.Database.ComisionAdapter _ComisionData;
+            public Data.Database.ComisionAdapter ComisionData
+        {
+                get { return this._ComisionData; }
+                set { this._ComisionData = value; }
+            }
+
+            public ComisionLogic(Data.Database.ComisionAdapter x)
+            {
+            ComisionData = x;
+            }
+
+            public ComisionLogic()
+            {
+            }
+
+            public List<Comision> GetAll()
+            {
+                this.ComisionData = new Data.Database.ComisionAdapter();
+                return this.ComisionData.GetAll();
+            }
+
+            public Business.Entities.Comision GetOne(int ID)
+            {
+                this.ComisionData = new Data.Database.ComisionAdapter();
+                return ComisionData.GetOne(ID);
+            }
+
+            public void Delete(int ID)
+            {
+                this.ComisionData = new Data.Database.ComisionAdapter();
+            ComisionData.Delete(ID);
+            }
+
+            public void Save(Business.Entities.Comision com)
+            {
+                this.ComisionData = new Data.Database.ComisionAdapter();
+            ComisionData.Save(com);
+            }
     }
 }
