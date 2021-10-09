797fb4eace09a0cf876784b31f4b192ecc412e82
AlexisTomas2000 <alexisjosetomas@gmail.com>
2021-10-09 09:53:05 -0300

    CambiosPersonas

--- Business.Logic/PersonaLogic.cs
+++ Business.Logic/PersonaLogic.cs
@@ -3,10 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Data;
+using System.Data.SqlClient;
+using Business.Entities;
 namespace Business.Logic
 {
     class PersonaLogic:BusinessLogic
     {
+        private Data.Database.PersonaAdapter _PersonaData;
+        public Data.Database.PersonaAdapter PersonaData
+        {
+            get { return _PersonaData; }
+            set { _PersonaData = value; }
+        }
+        public PersonaLogic()
+        {
+        }
+        public PersonaLogic(Data.Database.PersonaAdapter x)
+        {
+            this.PersonaData = x;
+        }
+        public List<Persona> GetAllP()
+        {
+            this.PersonaData = new Data.Database.PersonaAdapter();
+            return this.PersonaData.GetAllP();
+        }
+        public List<Persona> GetAllA()
+        {
+            this.PersonaData = new Data.Database.PersonaAdapter();
+            return this.PersonaData.GetAllA();
+        }
+        public Business.Entities.Persona GetOne(int ID)
+        {
+            this.PersonaData = new Data.Database.PersonaAdapter();
+            return PersonaData.GetOne(ID);
+        }
+
+        public void Delete(int ID)
+        {
+            this.PersonaData = new Data.Database.PersonaAdapter();
+            PersonaData.Delete(ID);
+        }
+
+        public void Save(Business.Entities.Persona per)
+        {
+            this.PersonaData = new Data.Database.PersonaAdapter();
+            PersonaData.Save(per);
+        }
+ 
     }
 }
--- Data.Database/Data.Database/PersonaAdapter.cs
+++ Data.Database/Data.Database/PersonaAdapter.cs
@@ -6,7 +6,7 @@ using System.Data;
 using System.Data.SqlClient;
 namespace Data.Database
 {
-    class PersonaAdapter : Adapter
+    public class PersonaAdapter : Adapter
     {
         public List<Persona> GetAllP()
         {
