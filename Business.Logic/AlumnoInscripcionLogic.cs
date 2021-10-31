fbb5b15a3062173a06c0b8a9c900b9b8180a0ef2
AlexisTomas2000 <alexisjosetomas@gmail.com>
2021-10-31 20:19:50 -0300

    pp

---
+++ Business.Logic/AlumnoInscripcionLogic.cs
@@ -0,0 +1,46 @@
+﻿using Business.Entities;
+using Data.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Logic
+{
+    public class AlumnoInscripcionLogic : BusinessLogic
+    {
+        private Data.Database.AlumnoInscripcionAdapter _AIData;
+
+        public AlumnoInscripcionAdapter AIData { get => _AIData; set => _AIData = value; }
+        public AlumnoInscripcionLogic()
+        {
+
+        }
+        public AlumnoInscripcionLogic(Data.Database.AlumnoInscripcionAdapter x)
+        {
+            this.AIData = x;
+        }
+        public List<AlumnoInscripcion> GetAll()
+        {
+            this.AIData = new Data.Database.AlumnoInscripcionAdapter();
+            return this.AIData.GetAll();
+        }
+        public Business.Entities.AlumnoInscripcion GetOne(int IDA, int IDC)
+        {
+            this.AIData = new Data.Database.AlumnoInscripcionAdapter();
+            return this.AIData.GetOne(IDA, IDC);
+        }
+        public void Delete(int IDA, int IDC)
+        {
+            this.AIData = new Data.Database.AlumnoInscripcionAdapter();
+            AIData.Delete(IDA,IDC);
+        }
+
+        public void Save(Business.Entities.AlumnoInscripcion AI)
+        {
+            this.AIData = new Data.Database.AlumnoInscripcionAdapter();
+            AIData.Save(AI);
+        }
+    }
+}
